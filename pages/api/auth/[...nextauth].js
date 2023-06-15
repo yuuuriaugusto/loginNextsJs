3438264e1d37be09384d9240e2b269af83f16f57
Yuri Augusto <yuuuriaugusto@gmail.com>
2023-06-15 20:36:36 -0300

    primeiro commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from 'next-auth/providers/github'
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectMongo from "@/database/conn";
+import Users from "@/model/Schema";
+import { compare } from "bcryptjs";
+
+export default NextAuth({
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
+      GithubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+      }),
+      CredentialsProvider({
+        name: 'Credentials',
+        async authorize(credentials, req){
+          connectMongo().catch(error => {error: 'Connection Failed...!'})
+
+          // check user existance
+          const result = await Users.findOne({email:credentials.email})
+          if(!result){
+            throw new Error('No user Found with Email Please Sing Up...!')
+          }
+
+          // compare
+          const checkPassword = await compare(credentials.password, result.password)
+
+          // incorrect password
+          if(checkPassword || result.email !== credentials.email){
+            throw new Error('Username or Password doesnt match');
+          }
+
+          return result;
+        }
+      })
+    ],
+    secret:'SL2MxXLBA8TvGvGz7E1bJRcGAU+RxYdhzBbFMWt+kt8='
+  })
